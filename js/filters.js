d5d96af6b61e51ef8d200cbe9c19aec4e7c38841
MarilyneD <marilynedestombes@gmail.com>
2022-04-29 18:25:50 +0200

    css recherche par filtre

--- js/filters.js
+++ js/filters.js
@@ -34,16 +34,17 @@ var selectedUstensilsArray = [];
 // fonctions d'ajout des options dans le menu select ingrédients 
 function addOptionsIngredients(recipesArray) {
   function ingredientsArrayInit() {
-  ingredientsArray = [];  
-  ingredientsArray =  recipesArray.map(recipe => recipe.ingredients).flat(); 
-  ingredientsArray = ingredientsArray.map(ingredient =>ingredient.ingredient); 
-  ingredientsArray = [...new Set(ingredientsArray)].sort(); 
-    };
+    ingredientsArray = [];
+    ingredientsArray = recipesArray.map(recipe => recipe.ingredients).flat();
+    ingredientsArray = ingredientsArray.map(ingredient => ingredient.ingredient);
+    ingredientsArray = [...new Set(ingredientsArray)].sort();
+  };
   ingredientsArrayInit();
-  ingredientsList.innerHTML = ""; 
-    function addOption(ingredient) {
-    ingredientsList.insertAdjacentHTML("beforeend", 
-    `<li class="ingredient-li">${ingredient}</li>`)};
+  ingredientsList.innerHTML = "";
+  function addOption(ingredient) {
+    ingredientsList.insertAdjacentHTML("beforeend",
+      `<li class="ingredient-li">${ingredient}</li>`)
+  };
   ingredientsArray.forEach((ingredient) => addOption(ingredient));
 }
 
@@ -56,15 +57,17 @@ function addOptionsAppliance(recipesArray) {
   }
   applianceArrayInit();
   appliancesList.innerHTML = "";
-  function addOption(appliance) {appliancesList.insertAdjacentHTML("beforeend", 
-  `<li class="appliance-li">${appliance}</li>`)}
+  function addOption(appliance) {
+    appliancesList.insertAdjacentHTML("beforeend",
+      `<li class="appliance-li">${appliance}</li>`)
+  }
   applianceArray.forEach((appliance) => addOption(appliance));
 }
 
-// de même pour le tableau des ustensils (plusieurs par recette)
+// de même pour le tableau des ustensiles (plusieurs par recette)
 function addOptionsUstensils(recipesArray) {
   function ustensilsArrayInit() {
-    ustensilsArray = recipesArray.map(recipe => recipe.ustensils).flat(); 
+    ustensilsArray = recipesArray.map(recipe => recipe.ustensils).flat();
     ustensilsArray = [...new Set(ustensilsArray)].sort();
   }
   ustensilsArrayInit();
@@ -87,17 +90,17 @@ function findRecipesContaining(string, recipesArray) {
     item.ingredients.some((ingredient) => exp.test(ingredient.ingredient)));
   reducedNameRecipesArray = recipesArray.filter((item) => exp.test(item.name));
   reducedDescriptionRecipesArray = recipesArray.filter((item) => exp.test(item.description));
-  reducedRecipesArray = reducedIngredientsRecipesArray.concat(reducedNameRecipesArray); 
+  reducedRecipesArray = reducedIngredientsRecipesArray.concat(reducedNameRecipesArray);
   reducedRecipesArray = reducedRecipesArray.concat(reducedDescriptionRecipesArray);
   reducedRecipesArray = [...new Set(reducedRecipesArray)];
 
   // un peu de console.log pour voir ce qui se passe !!!
   console.log("Un ingrédient contient l'expression,  numéros des recettes :",
-      reducedIngredientsRecipesArray.map(recipe => recipe.id));
+    reducedIngredientsRecipesArray.map(recipe => recipe.id));
   console.log("Le titre contient l'expression,  numéros des recettes :",
-        reducedNameRecipesArray.map(recipe => recipe.id));
+    reducedNameRecipesArray.map(recipe => recipe.id));
   console.log("La description contient l'expression,  numéros des recettes :",
-        reducedDescriptionRecipesArray.map(recipe => recipe.id));
+    reducedDescriptionRecipesArray.map(recipe => recipe.id));
   console.log("reducedRecipesArray", reducedRecipesArray);
 }
 
@@ -108,178 +111,177 @@ function findRecipesContaining(string, recipesArray) {
 function searchBarFunction() {
   searchBar.addEventListener("input", function (e) {
     ingredientsSearchBar.value = ""; //vide recherche textuelle des ingrédients
-    document.querySelectorAll(".tag").forEach((tag) => {tag.remove()}); //efface les tags lorsque l'on retourne sur une recherche par texte
+    document.querySelectorAll(".tag").forEach((tag) => { tag.remove() }); //efface les tags lorsque l'on retourne sur une recherche par texte
     let value = e.target.value;
     if (value.trim().length >= 3) {
       search.setAttribute("data-error1-visible", "false");
-      findRecipesContaining(value, recipesArray);      
-      if (reducedRecipesArray == 0 ){search.setAttribute("data-error2-visible","true");console.log("Aucune recette ne correspond");}
-      else{
-        search.setAttribute("data-error2-visible","false")
+      findRecipesContaining(value, recipesArray);
+      if (reducedRecipesArray == 0) { search.setAttribute("data-error2-visible", "true"); console.log("Aucune recette ne correspond"); }
+      else {
+        search.setAttribute("data-error2-visible", "false")
         gallery.innerHTML = "";
-      galleryBuilder(reducedRecipesArray);
-      addOptionsIngredients(reducedRecipesArray);
-      addOptionsAppliance(reducedRecipesArray);
-      addOptionsUstensils(reducedRecipesArray);
-      createIngredientsTag();
-      createApplianceTag();
-      createUstensilsTag();}
+        galleryBuilder(reducedRecipesArray);
+        addOptionsIngredients(reducedRecipesArray);
+        addOptionsAppliance(reducedRecipesArray);
+        addOptionsUstensils(reducedRecipesArray);
+        createIngredientsTag();
+        createApplianceTag();
+        createUstensilsTag();
+      }
     } else {
       gallery.innerHTML = "";
-    galleryBuilder(recipesArray);
-    addOptionsIngredients(recipesArray);
-    addOptionsAppliance(recipesArray);
-    addOptionsUstensils(recipesArray);
-    createIngredientsTag();
-    createApplianceTag();
-    createUstensilsTag();
-    search.setAttribute("data-error1-visible", "true");
-    search.setAttribute("data-error2-visible", "false");
-     }
+      galleryBuilder(recipesArray);
+      addOptionsIngredients(recipesArray);
+      addOptionsAppliance(recipesArray);
+      addOptionsUstensils(recipesArray);
+      createIngredientsTag();
+      createApplianceTag();
+      createUstensilsTag();
+      search.setAttribute("data-error1-visible", "true");
+      search.setAttribute("data-error2-visible", "false");
+    }
   });
 
 }
 
 
+//fonction qui écoute le click sur les ingrédients du menu select
+function createIngredientsTag() {
 
+  document.querySelectorAll(".ingredient-li").forEach((ingredientElement) => {
 
-  //fonction qui écoute le click sur les ingrédients du menu select
-  function createIngredientsTag() {
+    ingredientElement.addEventListener("click", () => {
+      console.log("click sur l'ingrédient :", ingredientElement.textContent);
 
-    document.querySelectorAll(".ingredient-li").forEach((ingredientElement) => {
-  
-      ingredientElement.addEventListener("click", () => {
-        console.log("click sur l'ingrédient :", ingredientElement.textContent);
-  
-        if (!(selectedIngredientsArray.includes(ingredientElement.textContent))) {
-          console.log("selectedIngredientsArray avant ajout", selectedIngredientsArray);
-          selectedIngredientsArray.push(ingredientElement.textContent);
-          console.log("selectedIngredientsArray", selectedIngredientsArray);
-          tags.insertAdjacentHTML("beforeend", `<div class="ingredient-tag tag">${ingredientElement.textContent}<img class="close-icon" src="assets/images/close-icon.svg" alt="croix fermer du tag" /></div>`);
-          tagRecipes();
-          gallery.innerHTML = "";
-          galleryBuilder(reducedTagRecipesArray);
-         addOptionsIngredients(reducedTagRecipesArray);
-         addOptionsAppliance(reducedTagRecipesArray);
-         addOptionsUstensils(reducedTagRecipesArray);
-         createIngredientsTag(); 
-         createApplianceTag();
-         createUstensilsTag();
-          closeTag();
-        } else { console.log("l'ingrédient a déjà été sélectionné") }
-      });
+      if (!(selectedIngredientsArray.includes(ingredientElement.textContent))) {
+        console.log("selectedIngredientsArray avant ajout", selectedIngredientsArray);
+        selectedIngredientsArray.push(ingredientElement.textContent);
+        console.log("selectedIngredientsArray", selectedIngredientsArray);
+        tags.insertAdjacentHTML("beforeend", `<div class="ingredient-tag tag">${ingredientElement.textContent}<img class="close-icon" src="assets/images/close-icon.svg" alt="croix fermer du tag" /></div>`);
+        tagRecipes();
+        gallery.innerHTML = "";
+        galleryBuilder(reducedTagRecipesArray);
+        addOptionsIngredients(reducedTagRecipesArray);
+        addOptionsAppliance(reducedTagRecipesArray);
+        addOptionsUstensils(reducedTagRecipesArray);
+        createIngredientsTag();
+        createApplianceTag();
+        createUstensilsTag();
+        closeTag();
+      } else { console.log("l'ingrédient a déjà été sélectionné") }
     });
-    
-  }
-    
-  //fonction qui écoute le click sur l'appareil' du menu select
-  function createApplianceTag() {
- 
-    document.querySelectorAll(".appliance-li").forEach((applianceElement) => {
-  
-      applianceElement.addEventListener("click", () => {
-        console.log("click sur l'appareil :", applianceElement.textContent);
-  
-        if (!(selectedApplianceArray.includes(applianceElement.textContent))) {
-          selectedApplianceArray.push(applianceElement.textContent);
-          console.log("selectedApplianceArray", selectedApplianceArray);
-          tags.insertAdjacentHTML("beforeend", `<div class="appliance-tag tag">${applianceElement.textContent}<img class="close-icon" src="assets/images/close-icon.svg" alt="croix fermer du tag" /></div>`);
-          tagRecipes();
-          gallery.innerHTML = "";
-          galleryBuilder(reducedTagRecipesArray);
-         addOptionsIngredients(reducedTagRecipesArray);
-         addOptionsAppliance(reducedTagRecipesArray);
-         addOptionsUstensils(reducedTagRecipesArray);
-         createIngredientsTag(); 
-         createApplianceTag();
-         createUstensilsTag();
-          closeTag();
-           } else { console.log("l'appareil a déjà été sélectionné") }
-      });
-    });
-  }
-    
-  //fonction qui écoute le click sur les ustensils du menu select
-  function createUstensilsTag() {
- 
-    document.querySelectorAll(".ustensil-li").forEach((ustensilElement) => {
-  
-      ustensilElement.addEventListener("click", () => {
-        console.log("click sur l'ustensile :", ustensilElement.textContent);
-  
-        if (!(selectedUstensilsArray.includes(ustensilElement.textContent))) {
-  
-          selectedUstensilsArray.push(ustensilElement.textContent);
-          console.log("selectedUstensilsArray", selectedUstensilsArray);
-          tags.insertAdjacentHTML("beforeend", `<div class="ustensil-tag tag">${ustensilElement.textContent}<img class="close-icon" src="assets/images/close-icon.svg" alt="croix fermer du tag" /></div>`);
-          tagRecipes();
-          gallery.innerHTML = "";
-          galleryBuilder(reducedTagRecipesArray);
-         addOptionsIngredients(reducedTagRecipesArray);
-         addOptionsAppliance(reducedTagRecipesArray);
-         addOptionsUstensils(reducedTagRecipesArray);
-         createIngredientsTag(); 
-         createApplianceTag();
-         createUstensilsTag();
-          closeTag();
-           } else { console.log("l'appareil a déjà été sélectionné") }
-      });
-    });
-  }
+  });
 
- //fonction qui écoute le click/close quand je ferme le TAG
- function closeTag() {
-  document.querySelectorAll(".tag").forEach((tag) => {
-     function reInitAll(){
-       tagRecipes();
-       gallery.innerHTML = "";
-       galleryBuilder(reducedTagRecipesArray);
-       addOptionsIngredients(reducedTagRecipesArray);
-       addOptionsAppliance(reducedTagRecipesArray);
-       addOptionsUstensils(reducedTagRecipesArray);
-       createIngredientsTag();
-       createApplianceTag();
-       createUstensilsTag();
-       closeTag();
-        };
-
-
-   tag.addEventListener("click", () => {
-    
-     switch (tag.classList[0]) { 
-  
-       case 'ingredient-tag':
-         selectedIngredientsArray = selectedIngredientsArray.filter(item => item !==  tag.textContent);
-         console.log("selectedIngredientsArray après suppression",selectedIngredientsArray);
-         tag.remove();
-         reInitAll();
-         break;
-       case 'appliance-tag':
-         console.log("selectedApplianceArray avant le click",selectedApplianceArray);
-         selectedApplianceArray = selectedApplianceArray.filter(item => item !==  tag.textContent);
-         console.log("selectedApplianceArray après suppression",selectedApplianceArray);
-         tag.remove();
-         reInitAll();
-         break;
-       case 'ustensil-tag':
-         selectedUstensilsArray = selectedUstensilsArray.filter(item => item !==  tag.textContent);
-         console.log("après suppression",selectedUstensilsArray);
-         tag.remove();
-         reInitAll();
-         break;
-       default:
-         console.log(`Sorry`);
-     }
-     
-   });
- });
 }
 
-  //fonction qui va reconstruire une liste de recettes selon les TAGS choisis
-  function tagRecipes() {
-      
-    if (reducedRecipesArray.length == 0) { reducedRecipesArray = [].concat(recipesArray) };
-    console.log("reducedRecipesArray avant TAGS", reducedRecipesArray);
+//fonction qui écoute le click sur l'appareil' du menu select
+function createApplianceTag() {
+
+  document.querySelectorAll(".appliance-li").forEach((applianceElement) => {
+
+    applianceElement.addEventListener("click", () => {
+      console.log("click sur l'appareil :", applianceElement.textContent);
+
+      if (!(selectedApplianceArray.includes(applianceElement.textContent))) {
+        selectedApplianceArray.push(applianceElement.textContent);
+        console.log("selectedApplianceArray", selectedApplianceArray);
+        tags.insertAdjacentHTML("beforeend", `<div class="appliance-tag tag">${applianceElement.textContent}<img class="close-icon" src="assets/images/close-icon.svg" alt="croix fermer du tag" /></div>`);
+        tagRecipes();
+        gallery.innerHTML = "";
+        galleryBuilder(reducedTagRecipesArray);
+        addOptionsIngredients(reducedTagRecipesArray);
+        addOptionsAppliance(reducedTagRecipesArray);
+        addOptionsUstensils(reducedTagRecipesArray);
+        createIngredientsTag();
+        createApplianceTag();
+        createUstensilsTag();
+        closeTag();
+      } else { console.log("l'appareil a déjà été sélectionné") }
+    });
+  });
+}
+
+//fonction qui écoute le click sur les ustensils du menu select
+function createUstensilsTag() {
+
+  document.querySelectorAll(".ustensil-li").forEach((ustensilElement) => {
+
+    ustensilElement.addEventListener("click", () => {
+      console.log("click sur l'ustensile :", ustensilElement.textContent);
+
+      if (!(selectedUstensilsArray.includes(ustensilElement.textContent))) {
+
+        selectedUstensilsArray.push(ustensilElement.textContent);
+        console.log("selectedUstensilsArray", selectedUstensilsArray);
+        tags.insertAdjacentHTML("beforeend", `<div class="ustensil-tag tag">${ustensilElement.textContent}<img class="close-icon" src="assets/images/close-icon.svg" alt="croix fermer du tag" /></div>`);
+        tagRecipes();
+        gallery.innerHTML = "";
+        galleryBuilder(reducedTagRecipesArray);
+        addOptionsIngredients(reducedTagRecipesArray);
+        addOptionsAppliance(reducedTagRecipesArray);
+        addOptionsUstensils(reducedTagRecipesArray);
+        createIngredientsTag();
+        createApplianceTag();
+        createUstensilsTag();
+        closeTag();
+      } else { console.log("l'appareil a déjà été sélectionné") }
+    });
+  });
+}
+
+//fonction qui écoute le click/close quand je ferme le TAG
+function closeTag() {
+  document.querySelectorAll(".tag").forEach((tag) => {
+    function reInitAll() {
+      tagRecipes();
+      gallery.innerHTML = "";
+      galleryBuilder(reducedTagRecipesArray);
+      addOptionsIngredients(reducedTagRecipesArray);
+      addOptionsAppliance(reducedTagRecipesArray);
+      addOptionsUstensils(reducedTagRecipesArray);
+      createIngredientsTag();
+      createApplianceTag();
+      createUstensilsTag();
+      closeTag();
+    };
+
+
+    tag.addEventListener("click", () => {
+
+      switch (tag.classList[0]) {
+
+        case 'ingredient-tag':
+          selectedIngredientsArray = selectedIngredientsArray.filter(item => item !== tag.textContent);
+          console.log("selectedIngredientsArray après suppression", selectedIngredientsArray);
+          tag.remove();
+          reInitAll();
+          break;
+        case 'appliance-tag':
+          console.log("selectedApplianceArray avant le click", selectedApplianceArray);
+          selectedApplianceArray = selectedApplianceArray.filter(item => item !== tag.textContent);
+          console.log("selectedApplianceArray après suppression", selectedApplianceArray);
+          tag.remove();
+          reInitAll();
+          break;
+        case 'ustensil-tag':
+          selectedUstensilsArray = selectedUstensilsArray.filter(item => item !== tag.textContent);
+          console.log("après suppression", selectedUstensilsArray);
+          tag.remove();
+          reInitAll();
+          break;
+        default:
+          console.log(`Sorry`);
+      }
+
+    });
+  });
+}
+
+//fonction qui va reconstruire une liste de recettes selon les TAGS choisis
+function tagRecipes() {
+
+  if (reducedRecipesArray.length == 0) { reducedRecipesArray = [].concat(recipesArray) };
+  console.log("reducedRecipesArray avant TAGS", reducedRecipesArray);
 
   reducedTagRecipesArray = [].concat(reducedRecipesArray);
   console.log("reducedTagRecipesArray avant TAGS", reducedTagRecipesArray);
@@ -313,22 +315,19 @@ function ingredientsSearchBarFunction() {
     allIngredientsArray.forEach((ingredientElement) => {
 
       if (ingredExp.test(ingredientElement)) {
-        console.log("Regex Test",ingredExp.test(ingredientElement));
+        console.log("Regex Test", ingredExp.test(ingredientElement));
         ingredientsList.insertAdjacentHTML("beforeend",
           `<li class="ingredient-li">${ingredientElement}</li>`)
       };
     }
-    
+
     )
     createIngredientsTag();
   }
-  
+
   )
 
 }
-
-
-
 
 
 function init() {
